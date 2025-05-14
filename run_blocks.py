8097ea56163616fa5519443a0d0e8fd0dff26fb1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-14 19:08:48 +0300

    Add minion.blk (string) and read it

--- run_blocks.py
+++ run_blocks.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import io
+import re
+
+BLOCK_RE = r"\[(?P<block>[^]]+\]"
+EMPTY_RE = r"^/{1,2}"
+minion_blk = """\
+[] [lightblue: Director]
+//
+[] [lightgreen: Secretary]
+//
+[Minion #1] [] [Minion #2]
+"""
 
 
 if __name__ == '__main__':
-    pass
+    with io.StringIO(minion_blk) as sh:
+        for line_no, line in enumerate(sh, 1):
+            print(f"{line_no = }, {line = }")
