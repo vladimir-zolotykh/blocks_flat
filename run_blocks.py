9dc13f4ce155c39eb67b32bf9c06b4946e3d2615
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-16 21:33:12 +0300

    Add Token, Separator classes

--- run_blocks.py
+++ run_blocks.py
@@ -18,22 +18,34 @@ minion_blk = """\
 """
 
 
-class Block:
-    def __init__(self, color: str, text: str, line_no: int = -1):
-        self.color = color
-        self.text = text
+class Token:
+    def __init__(self, line_no: int = -1):
         self.line_no = line_no
 
+
+class Separator(Token):
     def __repr__(self):
-        s: str = f"{self.color}: " if self.color else ""
-        return s + f"{self.text}"
+        return f"Separator(line_no={self.line_no})"
+
+
+class Block(Token):
+    def __init__(self, color: str, text: str, line_no: int = -1):
+        super().__init__(line_no=line_no)
+        self.color = color
+        self.text = text
+
+    def __repr__(self):
+        # s: str = f"{self.color}: " if self.color else ""
+        # return s + f"{self.text}"
+        return (f"Block(color={self.color}, text={self.text!r}, "
+                f"line_no={self.line_no})")
 
 
 def next_block(sh: TextIO) -> Generator[Block, None, None]:
     for line_no, line in enumerate(sh, 1):
         sep = re.match(SEPARATOR_RE, line)
         if sep:
-            continue            # skip "//"
+            yield Separator(line_no)
         for blk in re.finditer(BLOCK_RE, line):
             blk_text = blk.group('body')
             body = re.match(BODY_RE, blk_text)
