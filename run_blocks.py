08c7952dee136b230bda575b975b25fea1f66955
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-16 19:00:10 +0300

    Refactor next_block()
    Use BLOCK_RE, BODY_RE, SEPARATOR_RE regexps

--- run_blocks.py
+++ run_blocks.py
@@ -5,8 +5,10 @@ from typing import TextIO, Generator
 import io
 import re
 
-BLOCK_RE = r"\[(?P<block>[^]]+\]"
-EMPTY_RE = r"^/{1,2}"
+BLOCK_RE = r"\[(?P<body>[^]]*)\]"
+BODY_RE = r"(?P<color>[^:]+):\s*(?P<text>\w+)"
+SEPARATOR_RE = r"^/{1,2}$"
+
 minion_blk = """\
 [] [lightblue: Director]
 //
@@ -17,17 +19,26 @@ minion_blk = """\
 
 
 class Block:
-    def __init__(self, line: str, line_no: int):
-        self._line = line.rstrip()
-        self._line_no = line_no
+    def __init__(self, color: str, text: str, line_no: int = -1):
+        self.color = color
+        self.text = text
+        self.line_no = line_no
 
     def __repr__(self):
-        return (f"{self._line_no}: {self._line}")
+        return (f"{self.color}: {self.text}")
 
 
 def next_block(sh: TextIO) -> Generator[Block, None, None]:
     for line_no, line in enumerate(sh, 1):
-        yield Block(line, line_no)
+        sep = re.match(SEPARATOR_RE, line)
+        if sep:
+            continue            # skip "//"
+        for blk in re.finditer(BLOCK_RE, line):
+            blk_text = blk.group('body')
+            body = re.match(BODY_RE, blk_text)
+            if body:
+                color, text = body.groups()
+                yield Block(color, text, line_no=line_no)
 
 
 if __name__ == '__main__':
