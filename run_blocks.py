b93299198c84821d5f467a406847a93ee2e94328
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-18 18:31:35 +0300

    Rename Token to Node

--- run_blocks.py
+++ run_blocks.py
@@ -18,20 +18,22 @@ minion_blk = """\
 """
 
 
-class Token:
+class Node:
     def __init__(self, row: int, column: int, line_no: int):
         self.row = row
         self.column = column
         self.line_no = line_no
 
 
-class Separator(Token):
+class Separator(Node):
     def __repr__(self):
-        return (f"Separator(row={self.row}, column={self.column}, "
-                f"line_no={self.line_no})")
+        return (
+            f"Separator(row={self.row}, column={self.column}, "
+            f"line_no={self.line_no})"
+        )
 
 
-class Block(Token):
+class Block(Node):
     def __init__(self, color: str, text: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.color = color
@@ -40,12 +42,14 @@ class Block(Token):
     def __repr__(self):
         # s: str = f"{self.color}: " if self.color else ""
         # return s + f"{self.text}"
-        return (f"Block(color={self.color}, text={self.text!r}, "
-                f"row={self.row}, column={self.column}, "
-                f"line_no={self.line_no})")
+        return (
+            f"Block(color={self.color}, text={self.text!r}, "
+            f"row={self.row}, column={self.column}, "
+            f"line_no={self.line_no})"
+        )
 
 
-def next_block(sh: TextIO) -> Generator[Token, None, None]:
+def next_block(sh: TextIO) -> Generator[Node, None, None]:
     row: int = 0
     for line_no, line in enumerate(sh, 1):
         column: int = 0
@@ -53,7 +57,7 @@ def next_block(sh: TextIO) -> Generator[Token, None, None]:
         if sep:
             yield Separator(row, column, line_no)
         for blk in re.finditer(BLOCK_RE, line):
-            blk_text = blk.group('body')
+            blk_text = blk.group("body")
             body = re.match(BODY_RE, blk_text)
             if body:
                 color, text = body.groups()
@@ -62,7 +66,7 @@ def next_block(sh: TextIO) -> Generator[Token, None, None]:
         row += 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with io.StringIO(minion_blk) as sh:
         for block in next_block(sh):
             print(f"{block = }")
